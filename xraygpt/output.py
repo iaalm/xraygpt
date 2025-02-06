e08f13d06acbfe4a687990042d70fa59818876c4
iaalm <iaalmsimon@gmail.com>
2025-02-06 11:17:17 +0800

    output count

--- xraygpt/output.py
+++ xraygpt/output.py
@@ -2,10 +2,13 @@ from xraygpt.db.chroma import ChromaDatabase
 
 def printDatabase():
     db = ChromaDatabase(None, "workdir/db.chroma")
-    for i in db.dump():
+    data = db.dump()
+    for i in data:
         print(i["name"])
         print(i["description"])
         print("=" * 80)
+
+    print("Total items:", len(data))
 
 if __name__ == "__main__":
     printDatabase()
